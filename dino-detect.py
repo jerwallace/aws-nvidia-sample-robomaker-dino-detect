998bf9eb67a0f63765f6a34ea4d9c81c4c10f154
Jeremy Wallace <jeremygw@amazon.com>
2019-06-26 18:07:33 -0400

    incremental change

--- dino-detect.py
+++ dino-detect.py
@@ -68,11 +68,11 @@ def move_bot(image, robot_stop):
         x = xy[0]
         y = (0.5 - xy[1]) / 2.0
         angle = np.arctan2(x, y)
-        pid = angle * config.steering_gain_slider + (angle - angle_last) * config.steering_dgain_slider
+        pid = angle * config['steering_gain_slider'] + (angle - angle_last) * config['steering_dgain_slider']
         angle_last = angle
-        steering_slider = pid + config.steering_bias_slider
-        robot.left_motor.value = max(min(config.speed_gain_slider + steering_slider, 1.0), 0.0)
-        robot.right_motor.value = max(min(config.speed_gain_slider - steering_slider, 1.0), 0.0)
+        steering_slider = pid + config['steering_bias_slider']
+        robot.left_motor.value = max(min(config['speed_gain_slider'] + steering_slider, 1.0), 0.0)
+        robot.right_motor.value = max(min(config['speed_gain_slider'] - steering_slider, 1.0), 0.0)
 
 def dino_app():    
     global config, settings, device, logging
@@ -82,7 +82,7 @@ def dino_app():
     ddlogger = logging.getLogger('dino-detect')
     ddlogger.info("Set logger to ERROR mode.")
 
-    # Load in the log config files from a local config as well as the default greengrass config.
+    # Load in the log config files from a local config as well as the default greengrass config['
     ddlogger.info("Loading app config file...")
     with open('config.json') as json_config_file:
         config = json.load(json_config_file)
@@ -97,18 +97,18 @@ def dino_app():
     ddlogger.info("Starting cuda...")
     device = torch.device('cuda')
     ddlogger.info("Initializing robot on I2C Bus %i...", config['i2c_bus'])
-    robot = Robot(i2c_bus=config.i2c_bus)
-    camera = Camera.instance(width=config.image_size[0], height=config.image_size[1])
-    mean = config.np_value * np.array(config.mean_values)
-    stdev = config.np_value * np.array(config.std_values)
-    settings.mean_roadfollow = torch.Tensor(config.mean_values).cuda().half()
-    settings.std_roadfollow = torch.Tensor(config.std_values).cuda().half()
+    robot = Robot(i2c_bus=config['i2c_bus'])
+    camera = Camera.instance(width=config['image_size'][0], height=config['image_size'][1])
+    mean = config['np_value'] * np.array(config['mean_values'])
+    stdev = config['np_value'] * np.array(config['std_values'])
+    settings.mean_roadfollow = torch.Tensor(config['mean_values']).cuda().half()
+    settings.std_roadfollow = torch.Tensor(config['std_values']).cuda().half()
 
     ddlogger.info("Initializing ML models...")
     ddlogger.info("Road following model...")
     model_roadfollow = torchvision.models.resnet18(pretrained=False)
     model_roadfollow.fc = torch.nn.Linear(512, 2)
-    model_roadfollow.load_state_dict(torch.load(config.road_following_model))
+    model_roadfollow.load_state_dict(torch.load(config['road_following_model']))
     
     model = model_roadfollow.to(device)
     model = model_roadfollow.eval().half()
@@ -116,14 +116,14 @@ def dino_app():
     ddlogger.info("Dino detection model...")
     model_dinodet = torchvision.models.resnet18(pretrained=False)
     model_dinodet.fc = torch.nn.Linear(512, 6)
-    model_dinodet.load_state_dict(torch.load(config.dino_detect_model))
+    model_dinodet.load_state_dict(torch.load(config['dino_detect_model']))
 
     model_dinodet = model_dinodet.to(device)
     model_dinodet = model_dinodet.eval()
 
-    prev_class = config.prev_class
+    prev_class = config['prev_class']
 
-    # Initialize the AWS IoT Connection based on AWS Greengrass config. 
+    # Initialize the AWS IoT Connection based on AWS Greengrass config.
     ddlogger.info("Initializing AWS IoT...")
     logger = logging.getLogger("AWSIoTPythonSDK.core")
     streamHandler = logging.StreamHandler()
@@ -133,8 +133,8 @@ def dino_app():
     
     iotClient = None
     iotClient = AWSIoTMQTTClient('jetbot'+random.randint(1,101))
-    iotClient.configureEndpoint(gg_config.coreThing.iotHost, 8843)
-    iotClient.configureCredentials(gg_config.coreThing.caPath, gg_config.coreThing.keyPath, gg_config.coreThing.certPath)
+    iotClient.configureEndpoint(gg_config['coreThing.iotHost'], 8843)
+    iotClient.configureCredentials(gg_config['coreThing.caPath'], gg_config['coreThing.keyPath'], gg_config['coreThing.certPath'])
     iotClient.connect()
 
     ddlogger.info("Starting application loop...")
@@ -154,13 +154,13 @@ def dino_app():
                     "image": base64.b64encode(img)
                 }
             else:
-                ddlogger.info("Found %s...", config.dino_names[classes])
+                ddlogger.info("Found %s...", config['dino_names[classes])
                 message = {
-                    "dinosaur": config.dino_names[classes],
+                    "dinosaur": config['dino_names'][classes],
                     "confidence": str(probs),
                     "image": base64.b64encode(img)
                 }
-            iotClient.publish(config.topic, json.dumps(message), 1)
+            iotClient.publish(config['topic'], json.dumps(message), 1)
         move_bot(img, robot_stop)
 
 if __name__ == "__main__":
