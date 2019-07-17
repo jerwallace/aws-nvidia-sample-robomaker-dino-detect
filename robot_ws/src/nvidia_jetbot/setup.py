55fc757bdbefec138c787ae0f55b5e7897b47484
Jeremy Wallace <jeremygw@amazon.com>
2019-07-17 10:06:06 -0400

    latest

--- robot_ws/src/nvidia_jetbot/setup.py
+++ robot_ws/src/nvidia_jetbot/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['hello_world_robot'],
+    packages=['nvidia_jetbot'],
     package_dir={'': 'src'}
 )
 
