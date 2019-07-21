6137d4526ea3fe6179370d42e29cdfb78accb5c9
Jeremy Wallace <jeremygw@amazon.com>
2019-07-21 17:15:05 +0000

    auto build

--- robot_ws/src/dinobot/setup.py
+++ robot_ws/src/dinobot/setup.py
@@ -6,9 +6,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['dinobot'],
-    package_dir={'': 'src'},
-    install_requires=['Adafruit_SSD1306','AWSIoTPythonSDK','torch','torchvision','numpy','cv2','pil']
+    packages=['dinobot', 'Adafruit_SSD1306'],
+    package_dir={'': 'src'}
 )
 
 setup(**setup_args)
