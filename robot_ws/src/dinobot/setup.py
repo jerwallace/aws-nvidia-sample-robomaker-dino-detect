cdabfaa6fc7aea8db62fdd4a6253c2e73ccdc836
Jeremy Wallace <jeremygw@amazon.com>
2019-07-21 17:18:23 +0000

    auto build

--- robot_ws/src/dinobot/setup.py
+++ robot_ws/src/dinobot/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['dinobot', 'Adafruit_SSD1306'],
+    packages=['dinobot', 'Adafruit_Python_SSD1306'],
     package_dir={'': 'src'}
 )
 
