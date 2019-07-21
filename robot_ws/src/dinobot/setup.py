4c557caa3bcbd073139dbc7f95f4ff5aa2c4ebbd
Jeremy Wallace <jeremygw@amazon.com>
2019-07-21 13:18:29 +0000

    auto build

--- robot_ws/src/dinobot/setup.py
+++ robot_ws/src/dinobot/setup.py
@@ -7,7 +7,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['dinobot'],
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
+    install_requires=['Adafruit_SSD1306','AWSIoTPythonSDK']
 )
 
 setup(**setup_args)
