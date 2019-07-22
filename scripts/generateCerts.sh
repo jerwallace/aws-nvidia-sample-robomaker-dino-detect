616934c6aabdd7b0aac7791a39db1ca0c04d3723
Jeremy Wallace <jeremygw@amazon.com>
2019-07-22 21:53:29 +0000

    auto build

--- scripts/generateCerts.sh
+++ scripts/generateCerts.sh
@@ -2,8 +2,8 @@ CERTS_FOLDER=../robot_ws/src/dinobot/certs
 if ![ -d "$CERTS_FOLDER" ]; then
     mkdir $CERTS_FOLDER
 fi
-aws iot create-keys-and-certificate --set-as-active \
+OUTPUT=$(aws iot create-keys-and-certificate --set-as-active \
     --certificate-pem-outfile $CERTS_FOLDER/certificate.pem.crt \
-    --private-key-outfile  ../robot_ws/src/dinobot/certs/private.pem.key
-    --public-key-outfile  ../robot_ws/src/dinobot/certs/public.pem.key
-wget -O 
+    --private-key-outfile  $CERTS_FOLDER/private.pem.key
+    --public-key-outfile  $CERTS_FOLDER/public.pem.key)
+wget -O $CERTS_FOLDER/root.ca.pem wget -O $CERTS_FOLDER/root.ca.pem https://www.amazontrust.com/repository/AmazonRootCA1.pem
