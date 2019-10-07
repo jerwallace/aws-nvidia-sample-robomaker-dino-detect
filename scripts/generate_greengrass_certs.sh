3a9c9348d7c557c9d64a6150dedc395b6ba72c5b
A. Best <bestadam@amazon.com>
2019-10-07 17:33:32 +0000

    [doco] updates greengrass generate certs

---
+++ scripts/generate_greengrass_certs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
+CERTS_FOLDER=$BASE_DIR/robot_ws/src/dinobot/certs
+
+[ ! -d "$CERTS_FOLDER" ] && mkdir $CERTS_FOLDER
+
+OUTPUT=$(aws iot create-keys-and-certificate --set-as-active \
+    --certificate-pem-outfile "$CERTS_FOLDER/certificate.pem.crt" \
+    --private-key-outfile  "$CERTS_FOLDER/private.pem.key" \
+    --public-key-outfile  "$CERTS_FOLDER/public.pem.key")
+
+wget -O $CERTS_FOLDER/root.ca.pem wget -O $CERTS_FOLDER/root.ca.pem https://www.amazontrust.com/repository/AmazonRootCA1.pem
